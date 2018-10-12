f521ef3895a3c3cf9f5430e5733ab5bdc4ed6a92
Tom <devnull.ul@protonmail.com>
2018-10-12 16:19:29 -0400

    Added .desktop launcher file and icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "discord_player.h"
+#include "gpm_player.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    discord_player w;
+    gpm_player w;
     w.show();
 
     return a.exec();
